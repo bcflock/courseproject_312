7b20fcb12c5f77d1705f6e602bbb712e5bc81d6c
Benjamin Flock <59580773+bcflock@users.noreply.github.com>
2021-05-13 00:23:39 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 #ensure that you've already placed a kaggle.json file in your ~/.kaggle/ directory 
 
 module load python
+module load singularity
+
 pip install --user kaggle
 #The dataset is 27 gb large
 ssh dtn2 
@@ -10,3 +12,10 @@ kaggle datasets download -p ./data /sanglequang/brats2018
 logout
 #realistically, you should request a node for this job -- I just don't know what parameters affect zip speed
 unzip brats
+
+
+find . -name "*.nii" -type f -exec gzip {} \;
+
+singularity build tf.sif my-tf.def
+
+sbatch courseproject.slurm
